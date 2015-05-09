f85bfab2256ce5e299230a023f886d7a40e17d4c
Marcos Berssanette <berssa.net@gmail.com>
2015-05-09 16:44:44 -0300

    primeiro

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://192.168.0.15/ido-dev'
+  }, 
+
+  seedDB: true
+};
